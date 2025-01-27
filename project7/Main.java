5120e422d58a3e5ecd2f41041b90ed92af5e2e39
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-01-27 12:19:16 -0300

    date check

---
+++ project7/Main.java
@@ -0,0 +1,15 @@
+package project7;
+
+import java.util.Calendar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Calendar cal = Calendar.getInstance();
+
+        int HOUR = cal.get(Calendar.HOUR_OF_DAY);
+
+        System.out.println(HOUR);
+    }
+}
