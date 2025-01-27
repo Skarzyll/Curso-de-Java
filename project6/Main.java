65efa167f1977402c52ce4223aea5bc74382c0fd
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-01-27 12:11:33 -0300

    for

---
+++ project6/Main.java
@@ -0,0 +1,11 @@
+package project6;
+
+public class Main {
+    public static void main(String[] args) {
+        int n[] = {0, 1, 2, 3, 4, 5, 6};
+
+        for (int i = 0; i < n.length; i++) {
+            System.out.println(n[i]);
+        }
+    }
+}
