476b155c61d0a9cd2c127b9be801cf6f13d814bc
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-03 12:07:22 -0300

    method constructor

---
+++ project11/Caneta.java
@@ -0,0 +1,52 @@
+package project11;
+
+public class Caneta {
+    //atributos
+    private String  modelo;
+    private float   ponta;
+    private String  cor;
+    private boolean tampada;
+
+    //metodo construtor
+    public Caneta() {
+        this.cor = "azul";
+        this.tampada = true;
+    }
+
+    //metodos especiais
+    private String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void tampar() {
+        this.tampada = true;
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+
+    public void status() {
+        System.out.println("SOBRE A CANETA:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + (this.tampada == true ? "Sim" : "Não"));
+    }
+}
--- project11/Main.java
+++ project11/Main.java
@@ -2,6 +2,8 @@ package project11;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Caneta c1 = new Caneta();
+
+        c1.status();
     }
 }
