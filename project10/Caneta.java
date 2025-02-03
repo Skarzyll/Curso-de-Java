1f2498758b2a0f8662e561190eaf2f4e8c2e675e
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-03 09:20:04 -0300

    getters and setters

---
+++ project10/Caneta.java
@@ -0,0 +1,28 @@
+package project10;
+
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public void status() {
+        System.out.println("SOBRE A CANETA:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+    }
+}
--- project10/Main.java
+++ project10/Main.java
@@ -2,6 +2,11 @@ package project10;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Caneta c1 = new Caneta();
+
+        c1.setModelo("bic");
+        c1.setPonta(0.5f);
+
+        c1.status();
     }
 }
