8ef5c719ee606ddd0cc31c568185c2f07b4c479f
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-06 11:01:37 -0300

    add getters and setters

--- project12/Conta.java
+++ project12/Conta.java
@@ -5,5 +5,46 @@ public class Conta {
     private int     saldo;
     private boolean status;
 
+  
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     
 }
