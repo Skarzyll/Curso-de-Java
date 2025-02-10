2811fea919e1d89a7cb7bb8a0c7fc9bdcf44461d
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-10 11:32:53 -0300

    c2 Conta

--- project12/Main.java
+++ project12/Main.java
@@ -9,13 +9,29 @@ public class Main {
         c1.setSaldo(0);
         c1.depositar(1000);
         c1.pagarMensal();
+        c1.sacar(200);
 
-
-
-        System.out.println("nuimero da conta: " + c1.getNumConta());
+        System.out.println("numero da conta: " + c1.getNumConta());
         System.out.println("dono da conta: " + c1.getDono());
         System.out.println("status da conta: " + (c1.isStatus() ? "aberta" : "fechada"));
         System.out.println("tipo da conta: " + c1.getTipo());
         System.out.println("saldo da conta: " + c1.getSaldo());
+
+        Conta c2 = new Conta();
+
+        c2.setStatus(true);
+        c2.abrirConta("CP");
+        c2.setNumConta(2);
+        c2.setDono("Maritaca");
+        c2.setSaldo(0);
+        c2.depositar(1000);
+        c2.pagarMensal();
+        c2.sacar(100);
+
+        System.out.println("numero da conta: " + c2.getNumConta());
+        System.out.println("dono da conta: " + c2.getDono());
+        System.out.println("status da conta: " + (c2.isStatus() ? "aberta" : "fechada"));
+        System.out.println("tipo da conta: " + c2.getTipo());
+        System.out.println("saldo da conta: " + c2.getSaldo());
     }
 }
