73fe57ecb009e485fc86ecfa75971bdab2b85353
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-14 11:15:06 -0300

    create attributes, getters and setters

--- project13/ContoleRemoto.java
+++ project13/ContoleRemoto.java
@@ -1,5 +1,26 @@
 package project13;
 
 public class ContoleRemoto {
-
+    private  int      volume;
+    private  boolean  ligado;
+    private  boolean  tocando;
+    
+    public int getVolume() {
+        return volume;
+    }
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    public boolean isLigado() {
+        return ligado;
+    }
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+    public boolean isTocando() {
+        return tocando;
+    }
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
 }
