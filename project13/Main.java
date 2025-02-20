3eae742164a3a8c1358e5bc978c303453e401a4e
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-20 10:36:05 -0300

    call methods

--- project13/Main.java
+++ project13/Main.java
@@ -2,6 +2,15 @@ package project13;
 
 public class Main {
     public static void main(String[] args) {
-        
+        ControleRemoto c1 = new ControleRemoto();
+
+
+        c1.ligar();
+        c1.play();
+        for (int i = 0; i < 50; i++) {
+            c1.maisVolume();
+        }
+        c1.abrirMenu();
+        c1.fecharMenu();
     }
 }
