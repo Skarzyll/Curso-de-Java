c9b4664518f60dfadbec0c1480fd183a72e55667
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-21 11:40:38 -0300

    add interface

---
+++ project14/InteLutador.java
@@ -0,0 +1,9 @@
+package project14;
+
+public interface InteLutador {
+    abstract void apresentar();
+    abstract void status();
+    abstract void ganharLuta();
+    abstract void perderLuta();
+    abstract void empatarLuta();
+}
