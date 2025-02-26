6f7da2c4099760b4dd9c308632bbb1dd6da26d83
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-26 10:27:13 -0300

    first lutador

--- project14/Main.java
+++ project14/Main.java
@@ -2,8 +2,8 @@ package project14;
 
 public class Main {
     public static void main() {
-        int[] lutadores = {0, 1};
-        
-        lutadores[0] = new Lutador(null, null, 0, 0, 0, 0, 0, 0);
+        Lutador lutadores[] = new Lutador[1];
+
+        lutadores[0] = new Lutador("Caio", "Brasileiro", 21, 1.70f, 110.0f, 10, 5, 3);
     }
 }
