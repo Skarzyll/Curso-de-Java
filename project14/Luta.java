9e8113ad82ab41d5ac391d3416fa95fc76eb6d57
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-02-27 10:34:45 -0300

    create class Luta

---
+++ project14/Luta.java
@@ -0,0 +1,45 @@
+package project14;
+
+public class Luta{
+    private String  desafiado, desafiante;
+    private int     rounds;
+    private boolean aprovada;
+
+    public Luta() {
+        
+    }
+
+    public String getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(String desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public String getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(String desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+    
+}
