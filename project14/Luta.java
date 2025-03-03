f1c026465a907911aad4daaee74a71bb0da3ee11
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-03 16:16:35 -0300

    func marcarLuta

--- project14/Luta.java
+++ project14/Luta.java
@@ -41,8 +41,18 @@ public class Luta{
         this.aprovada = aprovada;
     }
 
-    public void marcarLuta() {
-
+    public void marcarLuta(Lutador L1, Lutador L2) {
+        if (L1.getCategoria() != L2.getCategoria()) {
+            if (L1 != L2) {
+                aprovada = true;
+                desafiado = L1;
+                desafiante = L2;
+            }
+        } else {
+            aprovada = false;
+            desafiado = null;
+            desafiante = null;
+        }
     }
 
     public void lutar() {
