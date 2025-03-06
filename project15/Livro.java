90dd4fe192efebf9a21abb232ce49d20fa7e19ad
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-06 10:27:21 -0300

    create class Livro and fixes class Pessoa

---
+++ project15/Livro.java
@@ -0,0 +1,57 @@
+public class Livro {
+    private String  titulo, autor, leitor;
+    private int     totpaginas, pagatual;
+    private boolean aberto;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(String leitor) {
+        this.leitor = leitor;
+    }
+
+    public int getTotpaginas() {
+        return totpaginas;
+    }
+
+    public void setTotpaginas(int totpaginas) {
+        this.totpaginas = totpaginas;
+    }
+
+    public int getPagatual() {
+        return pagatual;
+    }
+
+    public void setPagatual(int pagatual) {
+        this.pagatual = pagatual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public void detalhes() {
+        
+    }
+}
