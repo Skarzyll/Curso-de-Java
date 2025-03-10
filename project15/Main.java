c43930a6d4916cd35cd0d4c83cbf3f5411f3ed28
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-10 12:03:03 -0300

    intances

--- project15/Main.java
+++ project15/Main.java
@@ -4,5 +4,6 @@ public class Main {
         Livro[] l = new Livro[3]; 
 
         p[0] = new Pessoa("Caio", 21, "M");
+        l[0] = new Livro("Berserk", "Kentaro Miura", 370, 1, p[0], true);
     }
 }
