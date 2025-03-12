f7510d7af24fe1ede73e1899c252db779d611160
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-12 10:54:55 -0300

    getters and setters Professor

--- project16/Professor.java
+++ project16/Professor.java
@@ -1,8 +1,24 @@
 public class Professor extends Pessoa {
     private String especialidade;
-    private int    salario;
+    private float    salario;
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
 
     public void receberAum() {
-        
+
     }
 }
