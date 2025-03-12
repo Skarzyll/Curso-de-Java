399061f462226943c9484a1eaaee62ed37025dc5
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-12 10:26:34 -0300

    class Professor

--- project16/Pessoa.java
+++ project16/Pessoa.java
@@ -1,3 +1,8 @@
 public class Pessoa {
+    private String nome, sexo;
+    private int    idade;
 
+    public void fazerAniversario() {
+
+    }
 }
---
+++ project16/Professor.java
@@ -0,0 +1,8 @@
+public class Professor extends Pessoa {
+    private String especialidade;
+    private int    salario;
+
+    public void receberAum() {
+        
+    }
+}
