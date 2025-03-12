daff1a3d89046dbb630c7e0734d1d58ad22f41fa
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-12 10:55:58 -0300

    getters and setters Funcionario

--- project16/Funcionario.java
+++ project16/Funcionario.java
@@ -2,6 +2,22 @@ public class Funcionario {
     private String  setor;
     private boolean trabalhando;
 
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
+
     public void mudarTrabalho() {
         
     }
