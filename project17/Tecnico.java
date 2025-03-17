f27c4e9f8df79bc227bff4ff857fd1507dc5a1ed
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-17 11:34:11 -0300

    new class Tecnico

---
+++ project17/Tecnico.java
@@ -0,0 +1,15 @@
+public class Tecnico {
+    private String registroProfissional;
+
+    public String getRegistroProfissional() {
+        return registroProfissional;
+    }
+
+    public void setRegistroProfissional(String registroProfissional) {
+        this.registroProfissional = registroProfissional;
+    }
+
+    public void praticar() {
+        
+    }
+}
