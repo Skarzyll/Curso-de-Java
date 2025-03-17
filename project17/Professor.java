49b52e83540413e6b192df5f607c33e66e85aa5b
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-17 11:19:09 -0300

    Professor get and set

--- project17/Professor.java
+++ project17/Professor.java
@@ -1,3 +1,24 @@
 public class Professor {
+    private String especialista;
+    private int    salario;
 
+    public String getEspecialista() {
+        return especialista;
+    }
+
+    public void setEspecialista(String especialista) {
+        this.especialista = especialista;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public void aumSalario(int p) {
+        this.salario = this.salario + p;
+    }
 }
