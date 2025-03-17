2aea0c0d7d39408be992957427047d554771dc1a
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-17 11:21:02 -0300

    class Aluno

--- project17/Aluno.java
+++ project17/Aluno.java
@@ -1,3 +1,24 @@
 public class Aluno {
+    private String matricula;
+    private String curtso;
 
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurtso() {
+        return curtso;
+    }
+
+    public void setCurtso(String curtso) {
+        this.curtso = curtso;
+    }
+
+    public void pagarMensa() {
+        
+    }
 }
