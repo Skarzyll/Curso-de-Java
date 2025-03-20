3dd56848e47bae8222ccf72e3700ab731eebddaf
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-20 11:03:28 -0300

    class Aluno

--- project17/Aluno.java
+++ project17/Aluno.java
@@ -1,25 +1,34 @@
 //Subclass
 public class Aluno extends Pessoa{
     private int matricula;
-    private String curtso;
+    private String curso;
 
+    
     public int getMatricula() {
         return matricula;
     }
-
+    
     public void setMatricula(int matricula) {
         this.matricula = matricula;
     }
-
-    public String getCurtso() {
-        return curtso;
+    
+    public String getCurso() {
+        return curso;
     }
-
-    public void setCurtso(String curtso) {
-        this.curtso = curtso;
+    
+    public void setCurso(String curso) {
+        this.curso = curso;
     }
-
+    
     public void pagarMensa() {
         System.out.println("Ta pago!");
     }
+
+    @Override
+    public String toString() {
+        return "Aluno [matricula=" + matricula + ", curso=" + curso + ", getNome()=" + getNome() + ", getSexo()="
+                + getSexo() + ", getIdade()=" + getIdade() + "]";
+    }
+
+    
 }
--- project17/Main.java
+++ project17/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //Pessoa p1 = new Pessoa();
+
+        Aluno a1 = new Aluno();
+        a1.setNome("Carlos");
+        a1.setIdade(20);
+        a1.setSexo("M");
+        a1.setCurso("TI");
+        a1.setMatricula(111111);
+        a1.fazerAniversario();
+        a1.pagarMensa();
+        System.out.println(a1.toString());
+
         
     }
 }
