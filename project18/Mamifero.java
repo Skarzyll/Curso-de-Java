45e961ea2d4dbe336bcf64f211e3b42eb9bf7373
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-25 09:02:48 -0300

    getsets Mamifero

--- project18/Mamifero.java
+++ project18/Mamifero.java
@@ -1,3 +1,13 @@
 public class Mamifero extends Animal {
+    private String corPelo;
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+
     
 }
