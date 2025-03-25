e3ec56912261cb66a3c3fc492714eaff57e3cfa8
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-25 09:17:58 -0300

    getsets Repitil

--- project18/Repitil.java
+++ project18/Repitil.java
@@ -1,3 +1,11 @@
 public class Repitil {
+    private String corEscama;
 
+    public String getCorEscama() {
+        return corEscama;
+    }
+
+    public void setCorEscama(String corEscama) {
+        this.corEscama = corEscama;
+    }
 }
