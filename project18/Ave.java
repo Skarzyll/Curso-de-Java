bee7642908abd42d1f8b6b93ecbe6160967cad43
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-26 10:52:39 -0300

    getsets class Ave

--- project18/Ave.java
+++ project18/Ave.java
@@ -1,3 +1,15 @@
-public class Ave {
+public class Ave extends Animal {
+    private String corPena;
 
+    public void fazerNinho() {
+        
+    }
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
 }
