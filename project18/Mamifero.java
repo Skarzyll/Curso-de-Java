1a8b87291189fa585bf9e3b06e4b7291d856a018
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-26 10:58:22 -0300

    methods in Mamifero

--- project18/Mamifero.java
+++ project18/Mamifero.java
@@ -9,5 +9,18 @@ public class Mamifero extends Animal {
         this.corPelo = corPelo;
     }
 
-    
+    @Override
+    public void locomover() {
+        System.out.println("Correndo!");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Mamando");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de mamifero");
+    }
 }
