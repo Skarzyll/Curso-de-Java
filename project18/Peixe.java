5c1941a95325319f1cca71ab6c392459ce83074e
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-27 11:37:38 -0300

    funcs in class Peixe

--- project18/Peixe.java
+++ project18/Peixe.java
@@ -13,5 +13,18 @@ public class Peixe extends Animal {
         System.out.println("Bolhas");
     }
 
-    
+    @Override
+    public void locomover() {
+        System.out.println("Nadando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo algas");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de peixe");
+    }
 }
