84d74722eebe8573b35e3d15acdb9b630e69ac74
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-31 11:30:49 -0300

    funcs Cachorro

--- project18/Cachorro.java
+++ project18/Cachorro.java
@@ -1,3 +1,9 @@
 public class Cachorro extends Mamifero{
-    
+    public void guardarOsso() {
+        System.out.println("Guardou osso");
+    }
+
+    public void abanarRabo() {
+        System.out.println("Abanou Rabo");
+    }
 }
