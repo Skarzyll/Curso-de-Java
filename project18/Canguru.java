4b86892d1b39b9e53e7cdc2d56422a2925566b79
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-03-31 11:28:59 -0300

    funcs Canguru

--- project18/Canguru.java
+++ project18/Canguru.java
@@ -1,3 +1,10 @@
 public class Canguru extends Mamifero{
-    
+    public void usarBolsa() {
+        System.out.println("Guardou filhote");
+    }
+
+    @Override
+    public void locomover() {
+        System.out.println("Saltando");
+    }
 }
