0a246e0706efc866e1c8ed9990ec4ee69128a80c
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-01 14:44:14 -0300

    class Mamifero

---
+++ project19/Mamifero.java
@@ -0,0 +1,10 @@
+package project19;
+
+public class Mamifero extends Animal{
+    protected String corPelo;
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de mamifero");
+    }
+}
