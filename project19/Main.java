ae6d4c6b8a3808d4c7a8256950ca314d5408a633
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-01 14:52:56 -0300

    instance Mamifero

--- project19/Main.java
+++ project19/Main.java
@@ -2,6 +2,16 @@ package project19;
 
 public class Main {
     public static void main(String[] args) {
+        Mamifero m1 = new Mamifero();
+
+        m1.setCorPelo("Marrom");
+        m1.setIdade(7);
+        m1.setMembros(4);
+        m1.setPeso(70.7f);
+        m1.emitirSom();
+
+        System.out.println("------------%%--------------");
+
         
     }
 }
