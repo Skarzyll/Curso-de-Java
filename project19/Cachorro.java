dd267a59403b55bd0e4e5df57920a447a3af6899
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-01 18:13:10 -0300

    fuincs cachorro

--- project19/Cachorro.java
+++ project19/Cachorro.java
@@ -6,11 +6,42 @@ public class Cachorro extends Lobo{
         System.out.println("Auauauau");
     }
 
-    public void reagir(String frase) {}
+    public void reagir(String frase) {
+        if (frase.equals("toma comida") || frase.equals("ola")) {
+            System.out.println("abanar rabo e latir");
+        } else {
+            System.out.println("rosnar");
+        }
+    }
 
-    public void reagir(int hora, int min) {}
+    public void reagir(int hora, int min) {
+        if (hora < 12) {
+            System.out.println("abanar");
+        } else if (hora >= 18) {
+            System.out.println("ignorar");
+        } else {
+            System.out.println("abanar e latir");
+        }
+    }
 
-    public void reagir(boolean dono) {}
+    public void reagir(boolean dono) {
+        //String res = (dono == true) ? "abanar" : "rosnar e latira";
+        System.out.println((dono == true) ? "abanar" : "rosnar e latira");
+    }
 
-    public void reagir(int idade, float peso) {}
+    public void reagir(int idade, float peso) {
+        if (idade < 5) {
+            if (peso < 10) {
+                System.out.println("abanar");
+            } else {
+                System.out.println("latir");
+            }
+        } else {
+            if (peso < 10) {
+                System.out.println("rosnar");
+            } else {
+                System.out.println("ignorar");
+            }
+        }
+    }
 }
