30f4bd650dbda2fed44e51bf9e3cfcdfb277a9a9
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-01 16:20:22 -0300

    funcs reagir in class Cachorro

--- project19/Cachorro.java
+++ project19/Cachorro.java
@@ -5,4 +5,12 @@ public class Cachorro extends Lobo{
     public void emitirSom() {
         System.out.println("Auauauau");
     }
+
+    public void reagir(String frase) {}
+
+    public void reagir(int hora, int min) {}
+
+    public void reagir(boolean dono) {}
+
+    public void reagir(int idade, float peso) {}
 }
