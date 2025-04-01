3671cadeb1702addaf4808a1eaadf3c67e9b5214
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-01 14:53:18 -0300

    instance Cachorro

--- project19/Main.java
+++ project19/Main.java
@@ -12,6 +12,16 @@ public class Main {
 
         System.out.println("------------%%--------------");
 
+        Lobo l1 = new Lobo();
+
+        l1.setCorPelo("Marrom");
+        l1.setIdade(7);
+        l1.setMembros(4);
+        l1.setPeso(70.7f);
+        l1.emitirSom();
+
+        System.out.println("------------%%--------------");
+
         
     }
 }
