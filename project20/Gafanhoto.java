dbfccaab6fd00cf50a4b20764fc143e41ee6674b
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-03 15:50:11 -0300

    getterssetters in class Gafanhoto

--- project20/Gafanhoto.java
+++ project20/Gafanhoto.java
@@ -1,4 +1,21 @@
 public class Gafanhoto {
     private boolean login;
     private int     totAssistindo;
+
+    public boolean isLogin() {
+        return login;
+    }
+
+    public void setLogin(boolean login) {
+        this.login = login;
+    }
+
+    public int getTotAssistindo() {
+        return totAssistindo;
+    }
+
+    public void setTotAssistindo(int totAssistindo) {
+        this.totAssistindo = totAssistindo;
+    }
+
 }
