94126a5c3bec12231aade436937c6f9513dff999
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-03 15:33:15 -0300

    implements methods

--- project20/Video.java
+++ project20/Video.java
@@ -1,46 +1,55 @@
-public class Video {
+public class Video implements AcoesVideo {
     private String  title;
     private int     avaliacao, views, curtidas;
     private boolean reproduzindo;
-    
+
     public String getTitle() {
         return title;
     }
-    
+
     public void setTitle(String title) {
         this.title = title;
     }
-    
+
     public int getAvaliacao() {
         return avaliacao;
     }
-    
+
     public void setAvaliacao(int avaliacao) {
         this.avaliacao = avaliacao;
     }
-    
+
     public int getViews() {
         return views;
     }
-    
+
     public void setViews(int views) {
         this.views = views;
     }
-    
+
     public int getCurtidas() {
         return curtidas;
     }
-    
+
     public void setCurtidas(int curtidas) {
         this.curtidas = curtidas;
     }
-    
+
     public boolean isReproduzindo() {
         return reproduzindo;
     }
-    
+
     public void setReproduzindo(boolean reproduzindo) {
         this.reproduzindo = reproduzindo;
     }
-    
+
+    @Override
+    public void play() {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void like() {}
+
 }
