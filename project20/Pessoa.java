1cca6024c3ab9eeee4e3e28205af61670ee967a2
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-03 15:43:27 -0300

    getterssetters in super class Pessoa

--- project20/Pessoa.java
+++ project20/Pessoa.java
@@ -1,4 +1,36 @@
 public class Pessoa {
     protected String nome, sexo, expreciencia;
     protected int    idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getExpreciencia() {
+        return expreciencia;
+    }
+
+    public void setExpreciencia(String expreciencia) {
+        this.expreciencia = expreciencia;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
