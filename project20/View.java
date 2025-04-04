bacabb0801244a4e22c8187f7e19a73fad8d2c00
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-03 22:25:20 -0300

    getterssetters View

--- project20/View.java
+++ project20/View.java
@@ -2,5 +2,21 @@ public class View {
     private Gafanhoto espectador;
     private Video     video;
 
+    public Gafanhoto getEspectador() {
+        return espectador;
+    }
+
+    public void setEspectador(Gafanhoto espectador) {
+        this.espectador = espectador;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+    
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
 
 }
