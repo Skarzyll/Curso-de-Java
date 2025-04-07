b1736f9da0ad9ef9e2d295d5cba2b31e1c6cbd25
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-07 13:16:59 -0300

    add lutadores with ArrayList

--- project14/Main.java
+++ project14/Main.java
@@ -3,15 +3,17 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Lutador> lutadores = new ArrayList<Lutador>();
-        Lutador lutadores[] = new Lutador[2];
+        //Lutador lutadores[] = new Lutador[2];
         Luta pri = new Luta();
 
-        lutadores[0] = new Lutador("Caio", "Brasileiro", 21, 1.70f, 110f, 10, 5, 3);
+        //lutadores[0] = new Lutador("Caio", "Brasileiro", 21, 1.70f, 110f, 10, 5, 3);
+        lutadores.add(0, new Lutador("Caio", "Brasileiro", 21, 1.70f, 110f, 10, 5, 3));
 
-        lutadores[1] = new Lutador("Lucas", "Americano", 23, 1.65f, 111f, 5, 7, 8);
+        //lutadores[1] = new Lutador("Lucas", "Americano", 23, 1.65f, 111f, 5, 7, 8);
+        lutadores.add(1, new Lutador("Lucas", "Americano", 23, 1.65f, 111f, 5, 7, 8));
 
         pri.marcarLuta(lutadores[0], lutadores[1]);
-
+        
         pri.lutar();
     }   
 }
