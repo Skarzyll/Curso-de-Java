46a2faf006c05868798af70141714b74b0407b0c
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-14 12:29:19 -0300

    try

---
+++ Advanced/TryCatch/Main.java
@@ -0,0 +1,22 @@
+package Advanced.TryCatch;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        try {
+            a.add(10);
+            a.add(11);
+            a.add(12);
+            System.out.println(a.get(0));
+            System.out.println(a.get(1));
+            System.out.println(a.get(2));
+            System.out.println(a.get(10));
+        } catch (Exception e) {
+            System.out.println("erro de array " + e);
+        } finally {
+            System.out.println("finish");
+        }
+    }
+}
