5fa6b90febc2ed8623a0d182178b6f811f481b4e
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-16 18:04:53 -0300

    Comments in Thread

--- Advanced/Thread/Main.java
+++ Advanced/Thread/Main.java
@@ -1,9 +1,9 @@
 package Advanced.Thread;
 
-public class Main extends Thread {
+public class Main extends Thread { //Extende thread
     public static void main(String[] args) {
-        Main t = new Main();
-        t.start();
+        Main t = new Main(); //Cria uma variavel do tipo Main
+        t.start(); //Inicia a execucao do thread e chama a funcao run
         System.out.println("caio");
     }
 
