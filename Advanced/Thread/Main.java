02040b668ecee31bdc02441926b3229d9bd5fc06
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-16 18:10:46 -0300

    Implements Runnable

--- Advanced/Thread/Main.java
+++ Advanced/Thread/Main.java
@@ -1,9 +1,15 @@
 package Advanced.Thread;
 
-public class Main extends Thread { //Extende thread
+public class Main implements Runnable /* extends Thread */ { //Extende thread ou implementa o Runnable
     public static void main(String[] args) {
-        Main t = new Main(); //Cria uma variavel do tipo Main
-        t.start(); //Inicia a execucao do thread e chama a funcao run
+        Main m = new Main(); //Cria uma variavel do tipo Main
+
+        /* t.start(); */ //Inicia a execucao do thread e chama a funcao run
+
+        Thread t = new Thread(m); //Crio uma nova instancia de Thread e passo a variavel de main como parametro
+
+        t.start();
+        
         System.out.println("caio");
     }
 
