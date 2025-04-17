a2553fb595edccc8d66d7489de1b12d161eca11a
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-17 18:34:25 -0300

    More Thread

--- Advanced/Thread/Main.java
+++ Advanced/Thread/Main.java
@@ -1,6 +1,9 @@
 package Advanced.Thread;
 
 public class Main implements Runnable /* extends Thread */ { //Extende thread ou implementa o Runnable
+
+    public static int count = 0;
+
     public static void main(String[] args) {
         Main m = new Main(); //Cria uma variavel do tipo Main
 
@@ -9,11 +12,18 @@ public class Main implements Runnable /* extends Thread */ { //Extende thread ou
         Thread t = new Thread(m); //Crio uma nova instancia de Thread e passo a variavel de main como parametro
 
         t.start();
+
+        while (t.isAlive()) {
+            System.out.println("o thread ainda esta ativo");
+        }
         
-        System.out.println("caio");
+        System.out.println(count);
+        count++;
+        System.out.println(count);
     }
 
     public void run(){
         System.out.println("aaaaa");
+        count++;
     }
 }
