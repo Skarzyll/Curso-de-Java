f3fa49b06703b2fb99e9b470b5cda61ac5641a7a
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-18 14:24:24 -0300

    ifelse in ONE LINE

--- Advanced/Optional/Main.java
+++ Advanced/Optional/Main.java
@@ -6,13 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Optional<String> op = Optional.empty();
 
-        op = Optional.of("RISOSOSOSOS");
+        //op = Optional.of("kkkkk");
 
-        if (!op.isPresent()) {
+        //op.ifPresent( v -> System.out.println(v) );
+        System.out.println(op.orElseGet(() -> "RISOSOSOSOS"));
+
+        /* if (op.isEmpty()) {
             op = Optional.of("kkkk");
             System.out.println(op.get());
         } else {
             System.out.println(op.get());
-        }
+        } */
     }
 }
