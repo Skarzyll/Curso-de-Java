64e30c2d97bcf7bdb3b768c4b3302946b7d090ea
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2025-04-22 20:47:17 -0300

    Deque

---
+++ Advanced/Dequeue/Main.java
@@ -0,0 +1,12 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<Integer> deq = new LinkedList<Integer>();
+
+        deq.add(1);
+        deq.add(2);
+        
+    }
+}
